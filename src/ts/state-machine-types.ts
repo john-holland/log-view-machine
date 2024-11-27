775e4fb868a0b7ce2e262fbe422779df2121de7d
John Holland <john.gebhard.holland@gmail.com>
2024-11-26 20:50:49 -0500

    added graphql links

--- src/ts/state-machine-types.ts
+++ src/ts/state-machine-types.ts
@@ -51,6 +51,9 @@ type ViewMachine<TData extends any, TState extends State, TViewType extends any>
 }
 
 // @todd-o: todo: review: graphql implications beyond APIAdapters
+//     https://graphql.org/learn/authorization/
+//     https://graphql.org/learn/global-object-identification/
+// @todd-o: todo: review: graphql addressing scheme?
 
 // todo: add observables
 // type Machine { observeable: Observeable<State | undefined> }
